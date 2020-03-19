81e94ef922341c9357a2cd01f051cb479cbfa465
Laura Ghiorghisor <laura.ghiorghisor@gmail.com>
2020-03-19 18:49:01 +0000

    All checked. Changes made to creates: added addreses object table Changed order of table drops.

--- create.sql
+++ create.sql
@@ -9,22 +9,25 @@
 -- CREATE TYPES
 -- contact_type
 CREATE OR REPLACE TYPE contact_type AS OBJECT (
-    source VARCHAR2(20),
-    description VARCHAR2(100),
-    additional_details VARCHAR2(100));
+    contact_form VARCHAR2(20),
+    details VARCHAR2(100),
+    comments VARCHAR2(100));
 /
 
 -- contact_varray_type
-CREATE OR REPLACE TYPE contact_varray_type AS VARRAY(4) OF contact_type;
+CREATE OR REPLACE TYPE contact_varray_type AS VARRAY(10) OF contact_type;
+/
+-- date_type
+CREATE OR REPLACE TYPE date_varray_type AS VARRAY(5) OF DATE;
 /
 
 -- activity_type
 CREATE OR REPLACE TYPE activity_type AS OBJECT (
+    activity_id NUMBER(10),
     activity_name VARCHAR2(30),
-    cost NUMBER(6,2),
-    staff_needed VARCHAR2(200),
-    start_date DATE,
-    end_date DATE);
+    cost NUMBER(20,2),
+    no_staff_needed NUMBER(6),
+    activity_date date_varray_type);
 /
 SHOW ERRORS;
 
@@ -34,8 +37,7 @@ SHOW ERRORS;
 
 -- address_type
 CREATE OR REPLACE TYPE address_type AS OBJECT (
-    line1 VARCHAR2(50),
-    line2 VARCHAR2(70),
+    house_no VARCHAR2(30),
     street VARCHAR2(30),
     city VARCHAR2(25),
     county VARCHAR2(30),
@@ -45,6 +47,8 @@ CREATE OR REPLACE TYPE address_type AS OBJECT (
 SHOW ERRORS;
 
 -- CREATE TABLES
+
+
 CREATE TABLE sponsors (
     sponsor_id NUMBER(6),
     sponsor_firstname VARCHAR2(30),
@@ -52,63 +56,70 @@ CREATE TABLE sponsors (
     company_name VARCHAR2(60),
     address address_type,
     contact contact_varray_type,
-    registration_date DATE,
-    description VARCHAR2(200));
+    registration_date DATE);
+
+CREATE TABLE addresses OF address_type;
 
 CREATE TABLE locations (
     location_id NUMBER(6),
-    address address_type NOT NULL,
-    additional_notes VARCHAR2(200));
+    address REF address_type SCOPE IS addresses,
+    description VARCHAR2(200));
 
 CREATE TABLE experience_nature (
     experience_nature_id NUMBER(6),
     experience_nature_name VARCHAR2(30) NOT NULL,
-    comments VARCHAR2(200));
+    description VARCHAR2(200));
 
 CREATE TABLE experiences (
     experience_id NUMBER(6),
     experience_nature_id NUMBER(6) NOT NULL,
     experience_name VARCHAR2(30) NOT NULL,
     season VARCHAR2(20),
-    experience_date DATE,
+    experience_date date_varray_type,
     location_id NUMBER(6) NOT NULL,
     description VARCHAR2(200),
     activites activity_table_type)
-    NESTED TABLE activites STORE AS activity_table;
+    NESTED TABLE activites STORE AS activities_table;
 
 CREATE TABLE tickets (
     ticket_id NUMBER(6),
-    experience_id NUMBER(6) NOT NULL,
-    sponsor_id NUMBER(6) NOT NULL,
-    start_date DATE,
-    end_date DATE,
+    experience_id NUMBER(6),
+    sponsor_id NUMBER(6),
+    ticket_date date_varray_type,
     price NUMBER(6,2));
+
+
 
 -- CREATE SEQUENCES
 -- seq_sponsors
 CREATE SEQUENCE seq_sponsors
 INCREMENT BY 1
-START WITH 000001;
+START WITH 1;
 
 -- seq_location
 CREATE SEQUENCE seq_locations
 INCREMENT BY 1
-START WITH 000001;
+START WITH 1;
 
 -- seq_experience_nature
 CREATE SEQUENCE seq_experience_nature
 INCREMENT BY 1
-START WITH 000001;
+START WITH 1;
 
 -- seq_experiences
 CREATE SEQUENCE seq_experiences
 INCREMENT BY 1
-START WITH 000001;
+START WITH 1;
 
 -- seq_tickets
 CREATE SEQUENCE seq_tickets
 INCREMENT BY 1
-START WITH 000001;
+START WITH 1;
+
+--seq_activities
+CREATE SEQUENCE seq_activities
+INCREMENT BY 1
+START WITH 1;
 
 -- COMMIT CHANGES
 COMMIT;
