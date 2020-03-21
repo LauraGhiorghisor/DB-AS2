46e22c80918dec3ee39c71b4480a2b272db21dbb
Laura Ghiorghisor <laura.ghiorghisor@gmail.com>
2020-03-21 22:39:03 +0000

    JOINS and FUNCTIONS added

--- functions.sql
+++ functions.sql
@@ -6,7 +6,7 @@
 
 -- @C:\DB-AS2\functions.sql
 -- @/Users/Laura/csy2038/DB-AS2/functions.sql
-
+-- @C:\Users\Daiana\Desktop\functions.txt
 
 SET SERVEROUTPUT ON
 
@@ -31,6 +31,19 @@ END func_staff_total;
 /
 SHOW ERRORS;
 
+-- procedure to test func_staff_total
+CREATE OR REPLACE PROCEDURE proc_staff_total IS
+
+BEGIN 
+
+	DBMS_OUTPUT.PUT_LINE ('The total number of staff required for experience of id 1 is ' || func_staff_total(1));
+
+END proc_staff_total;
+/
+EXECUTE proc_staff_total
+
+
+
 
 -- func_duration
 CREATE OR REPLACE FUNCTION func_duration (in_start_date DATE, in_end_date DATE) RETURN NUMBER IS
@@ -47,26 +60,271 @@ END func_duration;
 /
 SHOW ERRORS;
 
+-- procedure to test func_duration
+CREATE OR REPLACE PROCEDURE proc_test_duration IS
+
+BEGIN 
+
+	DBMS_OUTPUT.PUT_LINE ('Duration is ' || func_duration('01-JAN-1991', '02-JAN-1991'));
+
+END proc_test_duration;
+/
+EXECUTE proc_test_duration
 
 
 
 
-CREATE OR REPLACE FUNCTION func_start_end_date (in_xp_id IN experiences.experience_id%TYPE, out_varray_date date_varray_type) RETURN NUMBER IS
 
-vn_months NUMBER(5);
+
+
+--Minimum price for an experience
+CREATE OR REPLACE FUNCTION func_xp_price_min
+RETURN NUMBER IS 
+      vn_price_min NUMBER(9,2);
+	
+	  BEGIN
+	       SELECT MIN(price)
+		   INTO vn_price_min
+		   FROM tickets;
+
+		   RETURN vn_price_min;
+
+	  END func_xp_price_min;
+/
+		   
+SHOW ERRORS;
+
+--Procedure to test function func_xp_price_min
+CREATE OR REPLACE PROCEDURE proc_xp_price_min IS
+
+    vn_min_price NUMBER(9,2);
+	vv_xp_name experiences.experience_name%TYPE;
 
 BEGIN
+    vn_min_price := func_xp_price_min;
+
+SELECT experience_name 
+INTO vv_xp_name
+FROM experiences 
+WHERE experience_id IN (
+	SELECT  experience_id
+	FROM tickets
+	WHERE price = vn_min_price);
+
+	DBMS_OUTPUT.PUT_LINE ('Minimum price is ' || vn_min_price || ' for the experience ' || vv_xp_name);
+END proc_xp_price_min;
+/
+EXECUTE proc_xp_price_min
+
+SHOW ERRORS;
+
+
+
+-- Total of tickets takings per experience
+
+CREATE OR REPLACE FUNCTION func_takings_total (in_xp_id IN experiences.experience_id%TYPE) 
+RETURN NUMBER IS
+
+vn_takings_total NUMBER(20);
+	  
+BEGIN
+SELECT SUM(price)
+INTO vn_takings_total
+FROM tickets
+WHERE experience_id = in_xp_id;
+		   
+RETURN vn_takings_total;
+
+END func_takings_total;
+/
+		  
+SHOW ERRORS;
+
+--Procedure to test function func_takings_total
+CREATE OR REPLACE PROCEDURE proc_xp_takings_total IS
+	
+BEGIN
+
+	DBMS_OUTPUT.PUT_LINE ('The current takings are  ' || func_takings_total(1) || ' for the given experience.');
+
+END proc_xp_takings_total;
+/
+EXECUTE proc_xp_takings_total
+
+SHOW ERRORS;
 
 
 
 
-vn_months := MONTHS_BETWEEN(in_start_date, in_end_date);
+-- Geat season based on date
+CREATE OR REPLACE FUNCTION func_season(in_date DATE)
+RETURN VARCHAR2 IS
+      vv_season experiences.season%TYPE;
+	  BEGIN
 
-DBMS_OUTPUT.PUT_LINE(vn_months);
-RETURN FLOOR(vn_months/24);
+	    IF EXTRACT(MONTH from in_date) IN (12,1,2) 
+		THEN vv_season := 'WINTER';
+		ELSIF EXTRACT(MONTH from in_date) IN (3,4,5) 
+		THEN vv_season := 'SPRING';
+		ELSIF EXTRACT(MONTH from in_date) IN (6,7,8) 
+		THEN vv_season := 'WINTER';
+		ELSE  vv_season := 'AUTUMN';
+		END IF;
 
-END func_start_end_date;
+	RETURN vv_season;
+
+END func_season;
 /
 SHOW ERRORS;
 
--- 
+-- must use CURSORS here
+
+
+-- CREATE OR REPLACE PROCEDURE proc_get_season(in_xp_id IN experiences.experience_id%TYPE) IS
+
+--     vc_date DATE;
+
+-- 	  BEGIN
+
+-- 	  date_varray DATE := DATE('01-MAR-2020', '01-APR-2021');
+-- 		-- SELECT d.*
+-- 		-- FROM experiences e, TABLE (e.activities) a, TABLE(a.activity_date) d)
+-- 		-- WHERE experience_id = in_xp_id);
+
+-- 	vc_date := date_varray(1);
+
+-- 	DBMS_OUTPUT.PUT_LINE ('The season for the given experience is ' || func_season(vc_date));
+	
+-- 	END proc_get_season;
+-- /
+-- EXEC proc_get_season(1);
+
+-- SHOW ERRORS;	  
+
+
+
+
+
+
+--Display country for a selected city
+
+CREATE OR REPLACE FUNCTION func_address_sponsors(in_address VARCHAR2) 
+RETURN VARCHAR2 IS
+      vc_location VARCHAR2(20);
+	  BEGIN
+           SELECT s.address.country
+	       INTO vc_location
+	       FROM sponsors s
+	       WHERE s.address.city = in_address;
+	       RETURN vc_location;
+	  END func_address_sponsors;
+/
+SHOW ERRORS;
+
+
+--Procedure to test function func_xp_price_min
+CREATE OR REPLACE PROCEDURE proc_address_sponsors(in_address VARCHAR2) IS
+vc_location VARCHAR2(20);
+	
+BEGIN
+    vc_location := func_address_sponsors(in_address);
+	DBMS_OUTPUT.PUT_LINE (vc_location);
+END proc_get_address;
+/
+EXEC proc_address_sponsors('KETTERING')
+
+SHOW ERRORS; 
+
+
+
+
+
+
+ 
+--Return number of activities in an experience
+CREATE OR REPLACE FUNCTION func_get_activities(in_experience_name IN experiences.experience_name%TYPE)
+RETURN NUMBER IS
+      vn_activities NUMBER(2);
+      BEGIN
+	     SELECT COUNT(a.activity_name)
+	     INTO vn_activities
+	     FROM experiences e,
+	     TABLE (e.activities) a
+	     WHERE e.experience_name = in_experience_name;
+	     RETURN vn_activities;
+	  END;
+/
+
+SHOW ERRORS;
+ 
+--Procedure to test function func_get_activities 
+CREATE OR REPLACE PROCEDURE proc_activities(in_experience_name IN experiences.experience_name%TYPE) IS
+vn_activities NUMBER(2);
+	
+BEGIN
+    vn_activities := func_get_activities(in_experience_name);
+	DBMS_OUTPUT.PUT_LINE ('There are ' || vn_activities || ' activities in the experience ' || in_experience_name);
+
+END proc_activities;
+/
+EXEC proc_activities('COMEDY NIGHT')
+
+SHOW ERRORS; 
+
+
+
+
+
+
+--Display description for a location
+CREATE OR REPLACE FUNCTION func_description_location(in_address VARCHAR2) 
+RETURN VARCHAR2 IS
+      vc_description VARCHAR2(200);
+	  BEGIN
+           SELECT l.description
+	       INTO vc_description
+	       FROM locations l
+	       WHERE l.address.city = in_address;
+	       RETURN vc_description;
+	  END func_description_location;
+/
+SHOW ERRORS;
+
+
+--Procedure to test function func_description_location
+CREATE OR REPLACE PROCEDURE proc_description_location(in_address VARCHAR2) IS
+vc_description VARCHAR2(200);
+	
+BEGIN
+    vc_description := func_description_location(in_address);
+	DBMS_OUTPUT.PUT_LINE (vc_description);
+END proc_description_location;
+/
+EXEC proc_description_location('BATH')
+
+SHOW ERRORS; 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- join.sql
+++ join.sql
@@ -6,3 +6,178 @@
 
 -- @C:\DB-AS2\join.sql
 -- @/Users/Laura/csy2038/DB-AS2/join.sql
+-- @C:\Users\Daiana\Desktop\joins.txt
+
+--Joins
+
+--2 Table Join
+SELECT l.location_id, l.address, e.experience_name, e.season
+FROM locations l
+JOIN experiences e
+ON l.location_id = e.location_id
+WHERE e.location_id = 1;
+--Returns 1 row
+
+--3 Table Join
+SELECT sponsors.sponsor_id, sponsors.sponsor_surname, experiences.experience_name
+FROM sponsors
+JOIN tickets
+ON sponsors.sponsor_id = tickets.sponsor_id
+JOIN experiences
+ON experiences.experience_id = tickets.experience_id;
+--Returns 5 rows
+
+--Full Outer Join
+SELECT experience_nature.experience_nature_id, experience_nature.experience_nature_name, experiences.experience_name, locations.location_id
+FROM experience_nature
+FULL OUTER JOIN experiences
+ON experience_nature.experience_nature_id = experiences.experience_nature_id
+FULL OUTER JOIN locations
+ON experiences.location_id = locations.location_id
+ORDER BY location_id;
+--Returns 5 rows
+
+--LEFT JOIN
+SELECT e.experience_id, e.experience_name, t.price
+FROM experiences e
+LEFT JOIN tickets t
+ON e.experience_id = t.experience_id
+WHERE price > 100.00
+AND e.experience_name LIKE 'H%';
+--Returns 1 row
+
+--Right Join
+SELECT en.experience_nature_name, e.experience_id, e.experience_name, e.activities
+FROM experiences e
+RIGHT JOIN experience_nature en
+ON e.experience_nature_id = en.experience_nature_id
+WHERE e.experience_id = 4;
+--Returns 1 row
+
+--UNION
+SELECT e.experience_id, e.location_id
+FROM experiences e
+UNION
+SELECT t.ticket_number, t.price
+FROM tickets t
+WHERE t.price < 300 AND t.price > 40;
+--7 rows returned
+
+SELECT sponsor_id
+FROM sponsors
+WHERE sponsor_surname LIKE 'D_'
+UNION
+SELECT ticket_number
+FROM tickets
+WHERE experience_id > 2
+AND price > 20;
+--Returns 6 rows
+
+SELECT house_no, postcode, country
+FROM addresses
+MINUS(
+      SELECT s.address.street, s.address.city, s.address.county
+	  FROM sponsors s
+	  WHERE s.address.county != 'CAMBRIDGESHIRE'
+	  UNION 
+	  SELECT l.address.street, l.address.city, l.address.county
+	  FROM locations l
+	  WHERE L.address.county != 'CAMBRIDGESHIRE');
+--Returns 5 rows
+
+SELECT house_no, street, postcode
+FROM addresses
+INTERSECT(
+          SELECT l.address.street, l.address.city, l.address.county
+		  FROM locations l 
+		  WHERE l.address.street = 'DOLBY STREET'
+		  UNION
+		  SELECT s.address.street, s.address.city, s.address.county
+	      FROM sponsors s);
+--No rows returned
+
+
+--SUB-QUERIES
+
+--Union based on address
+SELECT a.city, a.country
+FROM addresses a
+WHERE country NOT IN(
+     SELECT l.address.city
+	 FROM locations l
+	 UNION
+	 SELECT s.address.city
+	 FROM sponsors s
+	 WHERE country = 'UK');
+--5 rows returned
+
+--Exists value from a Join	 
+SELECT e.experience_id, e.activities
+FROM experiences e
+WHERE EXISTS(
+             SELECT t.ticket_number, t.price, s.sponsor_surname
+			 FROM sponsors s
+			 JOIN tickets t
+			 ON s.sponsor_id = t.sponsor_id
+			 WHERE t.price > 150
+			 AND s.sponsor_firstname IS NOT NULL);
+--3 rows returned
+
+--Sponsors not in Addresses		 
+SELECT s.sponsor_id, s.sponsor_surname, s.contact	
+FROM sponsors s
+WHERE s.address.city NOT IN(
+			         SELECT l.address.street
+                     FROM locations l
+					 WHERE l.address.country = 'UK')
+ORDER BY s.sponsor_id;
+--Returns 5 rows
+
+--Nested Varray query					 
+SELECT t.ticket_number, t.price
+FROM tickets t
+WHERE EXISTS(
+             SELECT s.sponsor_id, c.contact_form, c.details
+             FROM sponsors s,
+             TABLE (s.contact) c
+             WHERE s.sponsor_id = 3);	
+--Returned 6 rows			 
+					 
+SELECT VALUE (e)
+FROM THE(
+         SELECT activities
+		 FROM experiences
+		 WHERE experience_id = 1) e;
+--Returns 2 activity types
+             			 
+			 
+			 
+			 
+			 
+			 
+			 
+			 
+			 
+			 
+			 
+			 
+			 
+			 
+			 
+			 
+			 
+			 
+			 
+			 
+			 
+			 
+			 
+			 
+			 
+			 
+			 
+			 
+			 
+			 
+
+
